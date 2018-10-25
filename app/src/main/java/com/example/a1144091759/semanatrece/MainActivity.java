9411e32d6b9467db0987c5cf8d3ef05598b4649d
ManuelaE <manuelaepedesign@gmail.com>
2018-10-25 13:07:27 -0500

    Clase del jueves

--- app/src/main/java/com/example/a1144091759/semanatrece/MainActivity.java
+++ app/src/main/java/com/example/a1144091759/semanatrece/MainActivity.java
@@ -3,23 +3,47 @@ package com.example.a1144091759.semanatrece;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
+    //FirebaseAuth auth;
+    FirebaseDatabase db;
+    EditText etNombre, etCorreo, etPass;
+    Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+
+        etNombre = findViewById(R.id.et_nombre);
+        etCorreo = findViewById(R.id.et_correo);
+        etPass = findViewById(R.id.et_pass);
+        btnReg = findViewById(R.id.btn_reg);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Usuario usuario = new Usuario(etNombre.getText().toString(), etCorreo.getText().toString(), etPass.getText().toString());
+
+                db.getReference().child("usuarios").setValue(usuario);
+            }
+        });
+
+        /*auth = FirebaseAuth.getInstance();
 
         auth.createUserWithEmailAndPassword("manuelaepedesign@gmail.com", "12345678")
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -35,6 +59,6 @@ public class MainActivity extends AppCompatActivity {
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
-                });
+                });*/
     }
 }
---
+++ app/src/main/java/com/example/a1144091759/semanatrece/Usuario.java
@@ -0,0 +1,46 @@
+package com.example.a1144091759.semanatrece;
+
+class Usuario {
+
+    private String uid, nombre, correo, pass;
+
+    //clic derecho, generar, construcotr, 3 elementos
+    public Usuario(String nombre, String correo, String pass) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.pass = pass;
+    }
+
+    //clic derecho, generar, getters and setters
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
